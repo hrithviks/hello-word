78c32d0737432161faf39d7505962260c4e0a96e
Hrithvik S <hrithsnair90@gmail.com>
2025-07-28 18:32:23 +0800

    Add python script to test API gateway

---
+++ hello-word-random-word-api/exec_random_word_api.py
@@ -0,0 +1,62 @@
+import requests
+from requests_aws4auth import AWS4Auth
+import boto3
+
+# AWS Configuration
+AWS_REGION = "ap-southeast-1"
+AWS_API_GATEWAY_ENDPOINT = "https://ia9fkfvs02.execute-api.ap-southeast-1.amazonaws.com/demo/helloword/"
+AWS_RANDOM_WORD_API = "getRandomWord"
+TEST_PARAMS_MEDIUM = {'category': 'animals', 'difficulty': 'medium'}
+TEST_PARAMS_HARD = {'category': 'birds', 'difficulty': 'hard'}
+
+# Generate AWS credentials to sign using AWS4Auth
+session = boto3.Session()
+credentials = session.get_credentials()
+
+def invoke_api_gateway(method, path, headers=None, params=None, data=None, json=None):
+    """
+    Invokes an AWS API Gateway endpoint with AWS4Auth.
+    """
+
+    url = f"{AWS_API_GATEWAY_ENDPOINT}{path}"
+    auth = AWS4Auth(credentials.access_key, credentials.secret_key, AWS_REGION, 'execute-api', session_token=credentials.token)
+
+    with requests.Session() as session:
+        try:
+            response = session.request(
+                method,
+                url,
+                auth=auth,
+                headers=headers,
+                params=params,
+                data=data,
+                json=json
+            )
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException as e:
+            print(f"Error invoking API Gateway: {e}")
+            if hasattr(e, 'response') and e.response is not None:
+                print(f"Response content: {e.response.text}")
+
+if __name__ == "__main__":
+    print("*** Testing API Gateway Invocation ***")
+
+    # 1. Test Medium Difficulty for Animals Category
+    print("*** Requesting 'animals' (medium) ***")
+    
+    response_medium = invoke_api_gateway('GET',
+                                          AWS_RANDOM_WORD_API,
+                                          params=TEST_PARAMS_MEDIUM)
+    if response_medium:
+        print(f"Status Code: {response_medium.status_code}")
+        print(f"Response Body: {response_medium.json()}")
+
+    # 2. Test Hard Difficuly for Birds Category
+    print("*** Requesting 'birds' (medium) ***")
+    response_hard = invoke_api_gateway('GET',
+                                       AWS_RANDOM_WORD_API,
+                                       params=TEST_PARAMS_HARD)
+    if response_hard:
+        print(f"Status Code: {response_hard.status_code}")
+        print(f"Response Body: {response_hard.json()}")
