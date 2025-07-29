4d93e258c3cca22f56f56c5ec7a69aa95f492ff9
Hrithvik S <hrithsnair90@gmail.com>
2025-07-29 14:24:02 +0800

    Update module configuration to common location

---
+++ terraform-modules/iam/policies/main.tf
@@ -0,0 +1,10 @@
+/*
+Author      : Hrithvik Saseendran
+Description : Configuration for IAM Permissions and Roles
+*/
+
+resource "aws_iam_policy" "main" {
+  name        = var.iam_policy_name
+  description = var.iam_policy_description
+  policy      = var.iam_policy_json
+}
---
+++ terraform-modules/lambda/outputs.tf
@@ -0,0 +1,9 @@
+/*
+Author      : Hrithvik Saseendran
+Description : Outputs for Lambda functions
+*/
+
+output "lambda_arn" {
+  description = "The ARN of the created Lambda function"
+  value       = aws_lambda_function.main.arn
+}
