ea08626deb9fd3a9541ffe9e34f55e677d2ffe0b
Hrithvik S <hrithsnair90@gmail.com>
2025-08-21 23:50:34 +0800

    Update output for API Gateway module

--- terraform-modules/api-gateway/rest_api/outputs.tf
+++ terraform-modules/api-gateway/rest_api/outputs.tf
@@ -7,6 +7,10 @@ output "rest_api_id" {
   value = aws_api_gateway_rest_api.main.id
 }
 
+output "rest_api_name" {
+  value = aws_api_gateway_rest_api.main.name
+}
+
 output "rest_api_endpoint" {
   value = aws_api_gateway_rest_api.main.endpoint_configuration
 }
