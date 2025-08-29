741d05e19e39e4655c9951354b3d0bfa3ec913ce
Hrithvik S <hrithsnair90@gmail.com>
2025-08-29 17:08:35 +0800

    Frontend App Structure

---
+++ frontend-app/app.py
@@ -0,0 +1,4 @@
+'''
+Program    : app.py
+Decription : Python based front-end program to host the game application.
+'''
---
+++ frontend-app/terraform/main.tf
@@ -0,0 +1,40 @@
+/*
+Project     : Hello Word Game
+Service     : Backend Application
+Description : The terraform configuration for AWS resources for the backend application
+*/
+
+###################################
+# Get Centrally Managed Resources #
+###################################
+
+############################################
+# Cloudwatch log group for the ECS Cluster #
+############################################
+
+################################
+# Execution Role for ECS Tasks #
+################################
+
+###################################
+# IAM Permission policies for ECS #
+###################################
+
+# Cloudwatch
+# Backend ECS Cluster
+
+######################################################
+# Application Load Balancer for Frontend Application #
+######################################################
+
+###########################################
+# Security Group for Frontend ECS Cluster #
+###########################################
+
+#################
+# ECS Resources #
+#################
+
+##############################
+# Auto Scaling Group for ECS #
+##############################
